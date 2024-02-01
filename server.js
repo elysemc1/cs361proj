7f514bc6120bc7be1b248af5e41dc182b9e17fde
Ellie Cohen <elysemc1@gmail.com>
2024-01-31 16:28:08 -0800

    create basic server which serves list.html

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = 8080;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'list.html'));
