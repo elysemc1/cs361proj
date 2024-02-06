33959490aa92cdf56cb6021ff00f99d9cd133253
Elyse Cohen <elysemc1@gmail.com>
2024-02-05 19:00:35 -0800

    add task form works now

--- public/list.js
+++ public/list.js
@@ -4,24 +4,28 @@ function showTaskForm() {
 }
 
 function addTask() {
+    // get the input for all three elements
     var nameInput = document.getElementById("taskName");
-    var nameText = taskInput.value.trim();
+    var nameText = nameInput.value.trim();
 
     var dueInput = document.getElementById("dueDate");
     var dueText = dueInput.value.trim();
 
     var doInput = document.getElementById("doDate");
-    var doInput = doInput.value.trim();
+    var doText = doInput.value.trim();
 
+    // create an element in the todo-list list
     var todoList = document.getElementById("todo-list");
-
     var li = document.createElement("li");
-    // take care that inner HTML cannot be set by a user !! how to fix?
-    li.innerHTML = '<input type="checkbox"><span>' + nameText + '</span>';    
+    // take care that inner HTML cannot be set by a user
+    // user may only set text within the span 
+    li.innerHTML = '<input type="checkbox"><span>' + nameText + " | " + dueText + " | " + doText + '</span>';    
     todoList.appendChild(li);
     // clear fields for next time
     nameInput.value = "";
     dueInput.value = "";
     doInput.value = "";
+    // hide the menu again, until the user presses "add task"
+    document.getElementById('taskForm').hidden = true
 }
 
--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const path = require('path');
 
+// use express to serve the webpages
 const app = express();
 const port = 8080;
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'list.html'));
+    // the homepage, for now, will be /list
+    res.redirect('/list')
 });
+
+app.get('/list', (req, res) => {
+    // serve the HTML file that shows the user's to-do list
+    res.sendFile(path.join(__dirname, 'public', 'list.html'));
+})
+
+app.post('/list', (req, res) => {
+    // this will happen when we submit the form to add a task
+    // we don't actually want anything to happen, so simply continue
+})
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
